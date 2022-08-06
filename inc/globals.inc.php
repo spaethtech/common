74661dd1256676a6258365865f4cfda55df56f5b
Ryan Spaeth <rspaeth@spaethtech.com>
2022-08-05 21:17:58 -0500

    Added some globals.

---
+++ inc/globals.inc.php
@@ -0,0 +1,9 @@
+<?php /** @noinspection PhpUnused */
+declare(strict_types=1);
+
+if (!defined("PROJECT_DIR"))
+    define("PROJECT_DIR", realpath(__DIR__."/../../"));
+
+if (!defined("IDE_DIR"))
+    define("IDE_DIR", realpath(__DIR__."/../"));
+
