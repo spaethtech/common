84897b1ddc326e1d03c454fc0a495fb3d0fde9b3
Ryan Spaeth <rspaeth@spaethtech.com>
2022-08-05 21:57:41 -0500

    Setup for terminals

--- inc/globals.inc.php
+++ inc/globals.inc.php
@@ -2,8 +2,17 @@
 declare(strict_types=1);
 
 if (!defined("PROJECT_DIR"))
-    define("PROJECT_DIR", realpath(__DIR__."/../../"));
+{
+    // IF this script is nested in the vendor directory...
+    if (($vendor = realpath(__DIR__."/../../../")) && dirname($vendor) === "vendor")
+        // ..THEN this library has been required in another project, go up one directory!
+        define("PROJECT_DIR", realpath($vendor."/../"));
+    else
+        // ...OTHERWISE, this is the library itself!
+        define("PROJECT_DIR", realpath(__DIR__."/../"));
+}
 
-if (!defined("IDE_DIR"))
-    define("IDE_DIR", realpath(__DIR__."/../"));
+
+#if (!defined("IDE_DIR"))
+#    define("IDE_DIR", realpath(__DIR__."/../"));
 
